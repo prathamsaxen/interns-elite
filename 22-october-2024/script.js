45be685e6cdc9432621801f62872b8c460f1e638
prathamsaxen <er.prathamsaxena@gmail.com>
2024-10-22 21:00:44 +0530

    22 October 2024

---
+++ 22-october-2024/script.js
@@ -0,0 +1,151 @@
+// console.log("Connected");
+// // Array
+// // String
+// // Objects
+// // Functions
+// // Dom Manipulation
+// // Async Programming
+// // Try Catch Block - Errror handling
+
+// // const arr = [25, 20, 30, 35, 50];
+// // indexing   0    1   2   3   4
+
+// const arr2 = [25, 20, 30, 35, 50, 25, 20, 30, 35, 50];
+// //            0    1   2   3  4    5   6  7    8   9
+
+// // console.log(arr[2]);
+// // console.log(arr2[6]);
+
+// let sumOfAllStudentsMark=0;
+
+// for(var index=0;index<arr2.length;index++)
+// {
+//     sumOfAllStudentsMark += arr2[index];
+// }
+
+// // sumOfAllStudentsMark = sumOfAllStudentsMark + arr2[index];
+
+
+// console.log(sumOfAllStudentsMark/arr2.length);
+
+// // String Concatenation
+// let a=652;
+// let b=256;
+
+// console.log(a+b);
+
+// const stringData='Hey this is Pratham Saxena';
+
+// console.log(stringData[5]);
+
+// const human= {
+//     name:"Pratham Saxena",
+//     age:15,
+//     occupation:"Software Engineer"
+// }
+// sum('Hey am input Parameter');
+
+// // const data={
+// //     name:"Verna",
+// //     price:2000000,
+// //     m
+// // }
+// sum(28);
+
+// // let CarName="",
+// // let CarPrice=''
+
+// console.log(human.age);
+// console.log(human);
+
+// // Array - is a kind of similar or non similar collection of elements in a contnious memory allocation
+
+// // Functions - 
+// // 
+// function sum(inputParameter){
+//     console.log(inputParameter);
+//     console.log("Hey function called");
+//     return 'returned value';
+// }
+
+// function abc(){
+//     // code snippet - code to be execcuted
+// }
+
+// // Function declaration
+// // Function Definition
+
+// // Function call.
+
+// sum(8000000);
+// let value = sum();
+// console.log(value);
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+
+// // UTILS | HELPER
+
+// let Button = document.getElementById('btn-id');
+// function ButtonClick(){
+//     alert("Hey button is clicked");
+// }
+
+// Button.addEventListener('click',ButtonClick);
+
+// let initialValue=0;
+
+// document.getElementById('value').innerHTML=initialValue;
+
+
+// const PlusFunction=()=>{
+//     initialValue++;
+//     console.log(initialValue);
+//     document.getElementById('value').innerHTML=initialValue;
+// }
+
+// const MinusFunction=()=>{
+//     initialValue--;
+//     console.log(initialValue);
+//     document.getElementById('value').innerHTML=initialValue;
+
+// }
+
+
+// document.getElementById('pls-btn').addEventListener('click',PlusFunction);
+// document.getElementById('mns-btn').addEventListener('click', MinusFunction);
+
+// Single Threaded Language -
+// Whenever any process executes - one by one 
+// P5 - P2
+// P6 - P1
+
+// ASYNC - 
+// Time Taking Process -
+// Await - Keyword
+// Promis - successfull | failure
+
+// async function Greet(){
+//     let status = await console.log("Done");
+//     console.log(status);
+// }
+
+// Greet();
+
+// When you are using aysnc
+//  Promise - success | failure
+
+// try{
+    // ui - logged in
+// }
+// Catch(err)
+// {
+// error ("Inccorect Credentials")
+// }
